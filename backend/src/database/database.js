46971830289273b91573b6d11ac0b79b0bf86492
Daniel <allinasdaniel@gmail.com>
2025-07-10 23:50:18 +0800

    initial commit

---
+++ backend/src/database/database.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2';
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'myusername',
+    password: 'mypassword',
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected!');
+});
