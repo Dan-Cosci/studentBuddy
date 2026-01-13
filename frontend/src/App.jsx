481730495f99c2a92315443c5e9f1d9ed97498c6
Daniel <allinasdaniel@gmail.com>
2026-01-13 22:14:06 +0800

    studying structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import "./assets/css/style.css"
 
+import toast from 'react-hot-toast'
 import instance from './services/api.service.js'
 
 const App = () => {
 
   const buttonClick = () => {
+    toast.success("Hello world")
     instance.get('/notes/').then((res) => {
       console.log(res)
     })
   }
   
   return (
-    <div>
-      <h1>Hello world</h1>
+    <main className="main">
+      <h1 className=''>Hello world</h1>
       <button onClick={buttonClick}>Click me</button>
-    </div>
+    </main>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import toast ,{Toaster} from "react-hot-toast";
-import App from './App.jsx'
+import toast ,{ Toaster } from "react-hot-toast";
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <BrowserRouter>
+      <App />
+      <Toaster 
+        position="center-top"
+        reverseOrder={true}
+        containerStyle={{zIndex: 9999}}
+        />
+    </BrowserRouter>
   </StrictMode>,
 )
