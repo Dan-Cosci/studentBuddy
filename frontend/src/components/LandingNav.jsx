395568013c624c96da694383af2fba5d116bcf1f
Daniel <allinasdaniel@gmail.com>
2026-01-15 11:19:26 +0800

    landing page

--- frontend/src/components/LandingNav.jsx
+++ frontend/src/components/LandingNav.jsx
@@ -4,6 +4,17 @@ import { Link } from 'react-router-dom'
 const LandingNav = () => {
   return (
     <nav className="landing-nav">
+      <div className="landing-nav__logo-ct">
+        <div className="landing-nav__logo-ct__img">
+          <img src="./src/assets/images/logo.png" alt="" />
+        </div>
+        <div className="landing-nav__logo-ct__text">
+          <h1>Student
+            <span>Buddy</span>
+          </h1>
+        </div>
+      </div>
+
       <div className="landing-nav__link-ct">
         <ul className='landing-nav__ul'>
           <li className='landing-nav__li'><Link className='landing-nav__link' to="/">Home</Link></li>
