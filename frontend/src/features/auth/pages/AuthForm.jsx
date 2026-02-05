2ed7aa07af4b69e62b8d164c89a909b463b4f379
Daniel <allinasdaniel@gmail.com>
2026-02-05 15:49:37 +0800

    finised redesigning auth

--- frontend/src/features/auth/pages/AuthForm.jsx
+++ frontend/src/features/auth/pages/AuthForm.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { useOutletContext } from 'react-router-dom'
+import React, { useState }  from 'react'
+import { useOutletContext, Link } from 'react-router-dom'
 import './AuthForm.scss'
 
 const AuthForm = () => {
   const {mode,password, confirmPassword, setEmail, setPassword, setUsername, setConfirmPassword} = useOutletContext();
 
+  const [showpass, setShowpass] = useState(false);
   const match = password === confirmPassword;
 
   return (
@@ -20,16 +21,23 @@ const AuthForm = () => {
         <label htmlFor="email">Email</label>
       </div>
       <div className="form-body__field" >
-        <input placeholder='' className={`password ${!match ? 'invalid' : ''}`} type="password" name="password"  onChange={(e) => setPassword(e.target.value)}/>
+        <input placeholder='' className={`password ${!match ? 'invalid' : ''}`} type={`${showpass ? 'text' : 'password'}`} name="password"  onChange={(e) => setPassword(e.target.value)}/>
         <label htmlFor="password">Password</label>
       </div>
       {mode == 'register' && 
         <div className="form-body__field">
-          <input placeholder='' className={`password ${!match ? 'invalid' : ''}`} type="password" name="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} />
+          <input placeholder='' className={`password ${!match ? 'invalid' : ''}`} type={`${showpass ? 'text' : 'password'}`} name="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} />
           <label htmlFor="confirmPassword">Confirm Password</label>
         </div>
       }
+      <div className="form-body__additional">
+        <input type="checkbox" name="showPass" onChange={(e) => setShowpass(e.target.checked)}/>
+        <label htmlFor="showPass">show password</label>
+      </div>
       <button type="submit">{mode == 'register' ? 'Register' : 'Login'}</button>
+      <Link className='link' to={`/auth?mode=${mode == 'login'? 'register' : 'login'}`}>
+        {`${mode == 'login' ? 'Don\'t have an account? Register now!' : 'Have an account? Login!'}`}
+      </Link>
     </div>
   )
 }
